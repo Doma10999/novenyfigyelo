7967bee0d0e164edf093c2533e444b1e2886bc22
Doma10999 <152029437+Doma10999@users.noreply.github.com>
2026-01-21 19:43:29 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "novenyfigyelo-cache-v2";
+const CACHE_NAME = "novenyfigyelo-cache-v1";
 const OFFLINE_URL = "offline.html";
 
 self.addEventListener("install", (event) => {
@@ -6,24 +6,17 @@ self.addEventListener("install", (event) => {
     caches.open(CACHE_NAME).then((cache) =>
       cache.addAll([
         "index.html",
-        "offline.html",
-        "wifino.png"
+        "offline.html"
       ])
     )
   );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
-});
-
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     fetch(event.request).catch(() =>
-      caches.match(event.request).then((res) =>
-        res || caches.match(OFFLINE_URL)
-      )
+      caches.match(OFFLINE_URL)
     )
   );
 });
